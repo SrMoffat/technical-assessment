84043bf8a1310483fc5fb70488f4fe37dc5a2907
SrMoffat <ngigemoffat@gmail.com>
2024-09-21 01:50:57 +0300

    feat(layouts): add layouts

---
+++ frontend/app/layouts/public.tsx
@@ -0,0 +1,12 @@
+import { LandingPageFooter, LandingPageHeader } from "@/app/components/specific/landing"
+
+export default function UnauthenticatedLayout(props: any) {
+    const { children, hasHero } = props
+    return (
+        <>
+            <LandingPageHeader hasHero={hasHero} />
+            {children}
+            <LandingPageFooter />
+        </>
+    )
+}
