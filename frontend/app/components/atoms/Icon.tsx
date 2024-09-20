ed148d12cfcc173a85e82367db1c35e49a36d05d
SrMoffat <ngigemoffat@gmail.com>
2024-09-21 00:54:52 +0300

    feat(landing): add components bearbones for landing page and app

---
+++ frontend/app/components/atoms/Icon.tsx
@@ -0,0 +1,8 @@
+import Background from '@/app/assets/bg.png';
+import LogoWord from '@/app/assets/logo-word.svg';
+import LogoPng from '@/app/assets/logo.png';
+import Logo from '@/app/assets/logo.svg';
+import World from '@/app/assets/world.svg';
+
+export { Background, Logo, LogoPng, LogoWord, World };
+
