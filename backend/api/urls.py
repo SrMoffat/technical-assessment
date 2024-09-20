2669b902d468b4a33a42ffb6d0cdf20c4c250c5e
SrMoffat <ngigemoffat@gmail.com>
2024-09-20 08:47:53 +0300

    chore(setup): setup bearbones routes

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 
 from .views import api_health_check
 
-urlspatterns = [
+urlpatterns = [
     path('', api_health_check)
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,18 @@
+import json
 from django.http import JsonResponse
 
 
 def api_health_check(request, *args, **kwargs):
-    return JsonResponse({
-        "status": "ok"
+    body = request.body
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+
+    print("Data", {
+        "data": data,
+        "params": request.GET
     })
+
+    return JsonResponse(data)
