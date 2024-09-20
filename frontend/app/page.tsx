35425aaf9d2024ec634c583dd8565790d69c6795
SrMoffat <ngigemoffat@gmail.com>
2024-09-21 00:56:02 +0300

    chore(setup): add favicon

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,103 +1,125 @@
+import { Button, Flex } from 'antd';
 import Image from "next/image";
-import { Button } from 'antd';
+import Link from "next/link";
+import { Background, LogoWord, World } from "./components/atoms/Icon";
+
+
+export function Box() {
+  //  bg-dark-purple text-white  md:px-14 md:py-12 lg:p-16 flex flex-col gap-4 relative
+
+  // font-fraunces wonky  font-semibold lg:text-[3.5rem]  md:text-start
+  return (
+    <div className="p-8 mt-12">
+      <h1 className="text-[2rem] text-center font-semibold">Seamless and Efficient Data Handling</h1>
+      <p className="font-manrope text-center md:text-start">
+        Submit source and target files for reconciliation after data normalization to handle potential data transformation issues. Identify records present in source but missing in target and vice versa for accurate records.
+      </p>
+    </div>
+  );
+}
+
+interface EnumerableProps {
+  index: number;
+  title: string;
+  content: string;
+}
+
+export function Enumerable(props: EnumerableProps) {
+  return (
+    <div className="flex flex-col items-center py-10 gap-6 md:flex-row md:px-24 lg:flex-col">
+      <div className="rounded-full h-12 w-12 border border-white flex items-center justify-center font-fraunces">{props.index}</div>
+      <div className="flex flex-col items-center gap-4 px-4 md:items-start md:max-w-[520px] lg:max-w-full lg:items-center lg:px-0">
+        <h1 className="font-fraunces wonky text-[28px] font-semibold leading-[36px]">{props.title}</h1>
+        <p className="text-davys-grey font-manrope font-normal leading-[28px] text-center md:text-start lg:text-center">{props.content}</p>
+      </div>
+    </div>
+  )
+}
+
+
+function LandingPageHeader() {
+  return (
+    <header className="">
+      <nav className='flex items-center justify-between px-4 md:px-10 lg:px-40 lg:pt-14 pt-8'>
+        <div className="relative flex items-center h-[40px] w-[170px]">
+          <Image src={LogoWord} alt="Reconcili8" fill />
+        </div>
+        <div>
+          <Link className="hover:text-[#7078e8]" href="/login">Login</Link>
+          <Link href="/register">
+            <Button className="ml-4" type="primary">Register</Button>
+          </Link>
+        </div>
+      </nav>
+      <h1 className="text-white text-[40px] md:text-[50px] lg:text-[80px] font-fraunces leading-[50px] lg:leading-[80px]  w-full text-center font-semibold mt-16 md:mt-14 lg:mt-14">
+        <span className="underline">Reconciliate</span> financial <br />
+        records with <span className="underline">ease</span>.
+      </h1>
+      <Flex className="justify-center mt-12 md:text-red-500">
+        <Link href="/reconcile">
+          <Button size="large" className="ml-4" type="primary">Start Reconciliation</Button>
+        </Link>
+      </Flex>
+    </header>
+  )
+}
+
+function LandingPageContent() {
+  const items = [
+    {
+      index: 1,
+      title: 'Data Normalization',
+      content: 'Handles potential data transformation issues with date formats, case sensitivity, leading/trailing spaces, among others.'
+    },
+    {
+      index: 2,
+      title: 'Records Reconciliation',
+      content: 'Handles automated record reconciliation to identify missing records, discrepancies, and other validation errors that occur.'
+    },
+    {
+      index: 3,
+      title: 'Multi-Layered Approach',
+      content: 'Reconcialiation and validation checks happen on the client-side and server-side to ensure optimum experience and accurancy.'
+    }
+  ]
+
+  return (
+    <main className="pt-20">
+      <div className="bg-ghost-white flex flex-col lg:flex-row pt-[40px]">
+        {items.map((item) => (<Enumerable key={item.index} {...item} />))}
+      </div>
+      <div className="mt-8 flex flex-col items-center px-4 justify-center pt-10 ">
+        <div className="relative w-[360px] h-[280px]  sm:w-[640px] sm:h-[320px] md:w-[768px] md:h-[320px]">
+          <Image src={World} alt="background" fill />
+        </div>
+        <div className=''>
+          <Box />
+        </div>
+      </div>
+    </main>
+
+  )
+}
+
+function LandingPageFooter() {
+  return (
+    <footer className='flex flex-col items-center gap-2 py-20'>
+      <div className='relative h-[24px] w-[120px]'>
+        <Image src={LogoWord} alt="Reconcili8" fill />
+      </div>
+      <div className='flex justify-between'>
+        {new Date().getFullYear()}
+      </div>
+    </footer>
+  )
+}
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Button type="primary">Button</Button>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <>
+      <LandingPageHeader />
+      <LandingPageContent />
+      <LandingPageFooter />
+    </>
   );
 }
