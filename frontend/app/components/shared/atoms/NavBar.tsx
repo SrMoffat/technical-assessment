db136bcb51dbbc59925639e80a9875c53f1b044c
SrMoffat <ngigemoffat@gmail.com>
2024-09-21 13:07:16 +0300

    feat(home): extract navbar into component

---
+++ frontend/app/components/shared/atoms/NavBar.tsx
@@ -0,0 +1,66 @@
+"use client"
+
+import { LogoWordComponent } from '@/app/components/shared/atoms';
+import { AreaChartOutlined, DownOutlined, LogoutOutlined, UserOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Avatar, Dropdown, Flex, Layout, message, theme } from 'antd';
+import { useState } from "react";
+
+const { Header } = Layout;
+
+export default function NavBar() {
+    const [current, setCurrent] = useState('dashboard');
+
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
+
+    const onClick: MenuProps['onClick'] = ({ key }) => {
+        const isLogout = key === "logout"
+        setCurrent(key)
+
+        if (isLogout) {
+            message.error(`Click on item ${key}`);
+        } else {
+            message.info(`Click on item ${key}`);
+        }
+    };
+
+    const profileDropdownItems: MenuProps['items'] = [
+        {
+            label: 'Dashboard',
+            key: 'dashboard',
+            icon: <AreaChartOutlined />
+        },
+        {
+            label: 'View Profile',
+            key: 'profile',
+            icon: <UserOutlined />
+        },
+        {
+            label: 'Logout',
+            key: 'logout',
+            icon: <LogoutOutlined />
+
+        }
+    ]
+
+    return (
+        <Header
+            className="flex items-center justify-between"
+            style={{ backgroundColor: colorBgContainer }}
+        >
+            <LogoWordComponent />
+            <Dropdown menu={{ items: profileDropdownItems, onClick }}>
+                <Flex className="gap-2">
+                    <Avatar style={{ verticalAlign: 'middle' }
+                    } size="large">
+                        A
+                    </Avatar>
+                    < DownOutlined />
+                </Flex>
+            </Dropdown>
+        </Header>
+
+    )
+}
