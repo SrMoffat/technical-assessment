68c1983ecc10b1db85b93f3de4043be6f6bca302
SrMoffat <ngigemoffat@gmail.com>
2024-09-21 03:44:04 +0300

    feat(nav): link to roo route from logo

--- frontend/app/components/specific/landing/NavBar.tsx
+++ frontend/app/components/specific/landing/NavBar.tsx
@@ -4,7 +4,9 @@ import { Button } from "antd"
 export default function LandingPageNavBar() {
     return (
         <nav className='flex items-center justify-between px-4 md:px-10 lg:px-40 lg:pt-14 pt-8'>
-            <LogoWordComponent />
+            <CustomLink href="/">
+                <LogoWordComponent />
+            </CustomLink>
             <div>
                 <CustomLink className="hover:text-[#7078e8]" href="/login">Login</CustomLink>
                 <CustomLink href="/register">
