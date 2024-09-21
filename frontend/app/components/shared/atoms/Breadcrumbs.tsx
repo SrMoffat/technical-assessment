7307e2e12116e7bf9474810bc221a0ddbc799c47
SrMoffat <ngigemoffat@gmail.com>
2024-09-21 13:53:53 +0300

    fix(breadcrumbs): fix breacrumb errors

--- frontend/app/components/shared/atoms/Breadcrumbs.tsx
+++ frontend/app/components/shared/atoms/Breadcrumbs.tsx
@@ -1,14 +1,12 @@
 import { Breadcrumb } from 'antd';
 
-export default function Breadcrumbs(props: any){
-    const {items} = props
-
-    const steps = ["Home", ...items]
+export default function Breadcrumbs(props: any) {
+    const { items } = props
     return (
-        <Breadcrumb style={{ margin: '16px 0' }}>
-            {steps.map((entry) => (
-                <Breadcrumb.Item key={entry}>{entry}</Breadcrumb.Item>
-            ))}
-        </Breadcrumb>
+        <Breadcrumb style={{ margin: '16px 0' }} items={items.map((entry: any, index: number) => ({
+            key: entry,
+            href: '/reconcile',
+            title: entry,
+        }))} />
     )
 }
