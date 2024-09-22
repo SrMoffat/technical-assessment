f1fdc19a8260af2a2b371a6347c5663994988e41
SrMoffat <ngigemoffat@gmail.com>
2024-09-22 15:32:53 +0300

    chore(migrations): update models and run migrations

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,21 +4,6 @@ reconciliation_file_types = [('source', 'Source'), ('target', 'Target')]
 reconciliation_job_statuses = [(
     'pending', 'Pending'), ('completed', 'Completed'), ('failed', 'Failed')]
 
-class FileUpload(models.Model):
-    file_name = models.CharField(max_length=255)
-    file_type = models.CharField(
-        choices=reconciliation_file_types, max_length=100)
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-    # Add relationship to RecordEntry[] to serve as file contents instead of stroing raw file
-
-class Reconciliation(models.Model):
-    source_file = models.ForeignKey(
-        FileUpload, related_name='source_file', on_delete=models.CASCADE)
-    target_file = models.ForeignKey(
-        FileUpload, related_name='target_file', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(
-        max_length=100, choices=reconciliation_job_statuses)
 
 class RecordEntry(models.Model):
     external_id = models.CharField(max_length=255)
@@ -32,3 +17,24 @@ class RecordEntry(models.Model):
 
     class Meta:
         ordering = ['created_at']
+
+
+class FileUpload(models.Model):
+    file_name = models.CharField(max_length=255)
+    file_type = models.CharField(
+        choices=reconciliation_file_types, max_length=100)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    records = models.ForeignKey(
+        RecordEntry, related_name='entries', on_delete=models.CASCADE)
+
+
+class Reconciliation(models.Model):
+    source_file = models.ForeignKey(
+        FileUpload, related_name='source_file', on_delete=models.CASCADE)
+    target_file = models.ForeignKey(
+        FileUpload, related_name='target_file', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        'auth.User', related_name='reconciliations', on_delete=models.CASCADE)
+    status = models.CharField(
+        max_length=100, choices=reconciliation_job_statuses)
